3d8b4905f2c65a31c520a9c9355098f757262862
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 10:33:48 -0600

    Java 2 Learned to declare variables in Java.

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+//Learned to declare Java variables: undefined, null, boolean, string, symbol, number and object.
+//Variable names can be numbers, names, letters and $ or _ but cannot have spaces or start with a number.
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
