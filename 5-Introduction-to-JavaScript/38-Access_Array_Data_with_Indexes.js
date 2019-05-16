7cc68ee55537fd51a52c735a20f9e7420f9ee5c5
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 15:11:45 -0600

    Java 38 Learned to Access Array Data with Indexes.

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,9 @@
+//Learned to access array data with indexes. Ex: (variable)[n].
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+var myData = myArray[0]
+// Only change code below this line.
