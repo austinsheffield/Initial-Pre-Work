3744ed8d2a0ef882d174c26cbcad19cac84e56f7
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 13:51:19 -0600

    Java 27 Learned to Construct Strings with Variables.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+//Learned how to contruct a string with variables and the concatenation operator (+).
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Austin";
+var myStr = "My name is " + myName + " and I am well!";
