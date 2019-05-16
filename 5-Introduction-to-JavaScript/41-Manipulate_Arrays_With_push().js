fa7f883134c711e2c1464e65dcd64d9a3490f503
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 15:48:08 -0600

    Java 41 Learned to Manipulate Arrays w/ push()

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,12 @@
+//Learned how to manipulate arrays iwth push(). Code: (variable)push()
+//Make sure that if you are putting in multiple objects to include [].
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3]);
