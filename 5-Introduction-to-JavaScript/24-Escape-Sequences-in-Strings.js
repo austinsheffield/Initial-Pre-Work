7c2192b63fd4aae2fe899f046428238de7c0fc19
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 13:34:46 -0600

    Java 24 Learned more Escape Sequences in strings.

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,10 @@
+//Learned to use escape sequences in strings, which can be put into what the program says.
+// The code \' = single quote
+// The code \" = double quote
+// The code \\ = backslash
+// The code \n = newline
+// The code \r = carriage return
+// The code \t = tab
+// The code \b = backspace
+// The code \f = form feed
+var myStr = 'FirstLine\n\t\\SecondLine\nThirdLine'; // Change this line
