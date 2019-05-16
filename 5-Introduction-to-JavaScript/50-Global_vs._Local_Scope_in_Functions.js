a4ab97ab6664ad0943b7c7f8e2be18af82e0ee05
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 16:30:49 -0600

    Java  50 Learned Local outranks Global Scope.

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,14 @@
+//Local variables have precedence over Global variables.
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater" //should be striped sweater b/c I would wear that all the time.
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
