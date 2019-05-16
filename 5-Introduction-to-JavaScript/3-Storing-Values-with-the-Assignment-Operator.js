f80e74b8f53bc8c09d65e1421d6bc53a279db403
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 10:40:00 -0600

    Java 3 Learned to store variables with assignment operator.

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,9 @@
+//Learned to store variables with the assignment operator.
+//It is very important to note that Java assignments read right to left, opposite of the way of english.
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a = 7;
+b = a;
