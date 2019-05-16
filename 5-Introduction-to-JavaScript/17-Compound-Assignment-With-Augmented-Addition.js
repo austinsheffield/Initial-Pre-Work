102d5779f34eeaa941241c4d6dde6fd62c0bf6f3
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 12:22:56 -0600

    Java 17 Learned to use Compound Assignment with Augmated Addition.

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,10 @@
+//Learned to use compound assignment to do augmented addition. Basically simplifying code. Ex += adds the number after = to var.
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
