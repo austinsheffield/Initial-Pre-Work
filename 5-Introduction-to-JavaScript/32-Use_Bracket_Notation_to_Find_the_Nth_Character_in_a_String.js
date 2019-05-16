be4680d3f8c6ca19a79e9b829d30d19a0520e67a
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 14:18:47 -0600

    Java 32 Learned to use Bracket Notation to find characters in strings.

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+//Learned to use bracket notation to find the nth character in a string.
+//Important note: computers start at 0 so make sure to subract the number of characters by one.
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
