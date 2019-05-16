f746beaf9de4a8c43f9325da77662d55c10b79ba
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 16:16:43 -0600

    Java 47 Learned to Pass Values to Functions w/ Arguments.

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,13 @@
+//Learned how to pass values to functions with arguments.
+//Important to note that variables do matter. You cannot reuse a, b after you have already used it with another function.
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(c, d) {
+  console.log(c + d);
+}
+functionWithArgs(1 , 2);
