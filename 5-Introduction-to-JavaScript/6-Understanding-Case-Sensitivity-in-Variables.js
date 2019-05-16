678bf69de886122648823871e7597dc69c0916d2
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 10:53:58 -0600

    Java 6 Learned Java is case sensitive.

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+//Learned that Java is case sensitive and that I should use camelCase (Just like that) to name variables.
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
