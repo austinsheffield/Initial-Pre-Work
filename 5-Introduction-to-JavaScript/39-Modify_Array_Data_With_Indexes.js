28cedd3bb21a18eb9cb0ef16e965a9f2d2d7ffcc
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 15:14:09 -0600

    Java 39 Learned to Modify Arrays w/ Indexes.

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,10 @@
+//Learned that arrays can be changed unlike strings to have different variables.
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45;
