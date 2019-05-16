5e28267d579fbac7ce028a3af2760be1e295dc0f
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 13:21:20 -0600

    Java 22 Learned to use literal Quotes in Strings.

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,2 @@
+//Learned how to use quotes in strings. Interesting thing to note: while the first \" allows to use quotations, to close it you switch the two characters spaces: /'.
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
