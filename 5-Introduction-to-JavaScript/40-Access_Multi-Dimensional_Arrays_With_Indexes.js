76f7f4eb705c34991ba8c60f588a178b318c1041
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 15:44:32 -0600

    Java 40 Learned to Access Multi-Arrays w/ Indexes.

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,6 @@
+//Learned that I can access multi-dimensional arrays with indexes. Code: (variable)[n][n] where n refers to the point you are trying to reach.
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
