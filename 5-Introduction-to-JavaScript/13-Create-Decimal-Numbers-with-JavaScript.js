a56196870afdab946f927d997fa811e38904be35
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 11:55:22 -0600

    Java 13 Learned how to use decimal numbers.

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,6 @@
+//Learned how to create decimal numbers in Java. Not all real numbers can be represented in floating point (decimals).
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.7;
