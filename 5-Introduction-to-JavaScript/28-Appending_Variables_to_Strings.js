208f631ed8664ddd2ddb663745d1b6ade60b758f
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 13:54:47 -0600

    Java 28 Learned to Append Variables to Strings.

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,11 @@
+//Leared to build out a string over multiple lines with strings literals with the += operator.
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "fun!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
