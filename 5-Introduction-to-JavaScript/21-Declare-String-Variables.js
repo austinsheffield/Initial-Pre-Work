6d5c68bc0359ee0a27265a3fe366c0fc9b389f2b
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 13:15:48 -0600

    Java 21 Learned to declare String Variables.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,8 @@
+//Learned how to declare string variables and what a string literal is.(Ex: "your name")
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Austin";
+var myLastName = "Sheffield";
