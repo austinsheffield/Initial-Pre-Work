1956bb241e27584826d91eadc70c72af707ea883
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 14:13:24 -0600

    Java 31 Learned that Strings are Immutable.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+//Learned that strings once made, cannot be altered. You can change the variable to a different string but you have to reestablish the variable. Ex down below:
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
