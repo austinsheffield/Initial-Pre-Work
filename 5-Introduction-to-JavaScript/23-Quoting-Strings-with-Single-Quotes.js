b089ee27dd17892db987b44741249cb2aa00c548
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 13:26:02 -0600

    Java 23 Learned to quote strings with Single Quotes.

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,3 @@
+//Learned to quote strings with single quotes. Double quotes can be used without \ in single quotes.
+//Single and double quotes work the same in Java. ('=")
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
