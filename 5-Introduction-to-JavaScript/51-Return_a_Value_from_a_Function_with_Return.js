ab247c0980bf2f0dc82a1f496cc87614b5b72211
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 17:56:05 -0600

    Java 51 Learned to Return Values from Functions w/ Return.

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,13 @@
+//Learned how to return a value from a function with a return. Code: return num (and whatever you want to do to that variable).
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+
+console.log(minusSeven(10));
