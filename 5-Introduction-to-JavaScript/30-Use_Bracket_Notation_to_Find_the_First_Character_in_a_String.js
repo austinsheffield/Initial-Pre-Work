ca28e4cf1c512828901489b168f6a2d8dd2ef1c6
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 14:10:33 -0600

    Java 30 Learned to use Bracket Notation

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,13 @@
+//Learned how to use bracket notation to symplify the code to find the first character in the string. (Code: [0])
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
