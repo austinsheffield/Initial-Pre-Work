a9e5888e2fa10433e43a6e9050f519026428f6e9
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 12:14:47 -0600

    Java 16 Learned to find remainders.

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,4 @@
+// Learned how to find a remainder in Java and that the remainder operator is sometimes incorrectly reffered to as a modulus operator.
+// Only change code below this line
+
+var remainder= 11 % 3;
