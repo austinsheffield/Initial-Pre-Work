fcd6345b31f3c3fc924aef5ee294671fd95fcb83
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 19:57:27 -0600

    Java 71 Practiced Chaining If Else Statements.

---
+++ 5-Introduction-to-JavaScript/71-Chaining_If_Else_Statements.js
@@ -0,0 +1,23 @@
+//Practiced/Learned chaining if else statements. Gotta think like a machine.
+function testSize(num) {
+  // Only change code below this line
+  if (num >= 20) {
+    return "Huge";
+  }
+  else if (num < 5) {
+    return "Tiny";
+  }
+  else if (num < 10) {
+    return "Small";
+  }
+  else if (num < 15) {
+    return "Medium";
+  }
+  else {
+    return "Large";
+  }
+  // Only change code above this line
+}
+
+// Change this value to test
+testSize(7);
