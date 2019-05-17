eee75386b695e323d86b006ff494153bf0cf6070
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 19:28:08 -0600

    Java 67 Learned Comparisons w/ Logical Or Operator.

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,13 @@
+//Learned to use the logical or operator (||) which returns true if either side are true.
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val < 10) {
+    return "Outside";
+  }
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(21);
