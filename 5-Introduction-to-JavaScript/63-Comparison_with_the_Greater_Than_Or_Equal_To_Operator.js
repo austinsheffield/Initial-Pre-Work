f53c3f19b5145e0c1f9e1b7f9fd943e28f86cda0
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 19:13:44 -0600

    Java 63 Learned to Compare w/ Greater Than Or Equal To

---
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,15 @@
+//Learned to use the greater than or equal to operator, which will comvert data types.
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
