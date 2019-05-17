60a7020a67b23d6b7bd7bc6fb9f0da3abb51a96d
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 19:05:35 -0600

    Java 60 Learned to Compare values with Inequality Operator.

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+//Learned about the inequality operator (!=) and how to use it. It returns true if false and false if true. Doesnt care what data type of value.
+//Think of it like a computer thinks of it: 1 = on = true / and / 0 = off = false
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
