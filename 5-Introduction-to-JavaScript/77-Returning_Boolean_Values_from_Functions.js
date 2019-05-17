440d389ca02d1668a003ffb32e045795131df0ec
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 21:02:39 -0600

    Java 77 Learned that Return can be used to Simplify if/else

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,8 @@
+//Learned that I can use return as a boolean (true/false) to make if statements simpler
+function isLess(a, b) {
+  // Fix this code
+ return a <= b;
+}
+
+// Change these values to test
+isLess(10, 15);
