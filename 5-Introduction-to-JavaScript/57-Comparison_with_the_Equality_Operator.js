28e7ca750519c6d85bbe75ffa627fa09c341525e
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 18:48:40 -0600

    Java 57 Learned to Compare with Equality Operator.

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+//Learned to compare values with tht equality operator to symplify.
+//== does not equal =. == compares values and returns true or false, whereas = assigns values.
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(12);
