bc47d0e9521774c716ad80c211d611c335aeaf22
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 20:46:12 -0600

    Java 75 Learned to use Multiple Cases within same Switch.

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -1,1 +1,30 @@
+//Learned that if I dont use break; that the program will execute forever. Learned that I can run multiple casees within the same switch.
+function switchOfStuff(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+    return "apple";
+    break;
+  }
+  switch (val) {
+    case "b":
+    return "bird";
+    break;
+  }
+  switch (val) {
+    case "c":
+    return "cat";
+    break;
+    default:
+    return "stuff";
+    break;
 
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+switchOfStuff(1);
