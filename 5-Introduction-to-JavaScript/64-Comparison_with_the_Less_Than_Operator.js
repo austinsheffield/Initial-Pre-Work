c8dd4dd0ef2bc32c623f59307631e5c1366e2148
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 19:15:04 -0600

    Java 64 Learned the Less Than Operator.

---
+++ 5-Introduction-to-JavaScript/64-Comparison_with_the_Less_Than_Operator.js
@@ -0,0 +1,15 @@
+//Learned to compare with less than operator. Will convert.
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+// Change this value to test
+testLessThan(10);
