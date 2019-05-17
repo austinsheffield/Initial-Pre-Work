5a81b1efcd686479d3e737db369c8e62184daa67
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 19:31:33 -0600

    Java 68 Learned about Else Statements.

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,18 @@
+//Learned to use else statements which follow if statements and allow for simplifications.
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
