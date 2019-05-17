70ed45069064d5094266be974b70f23598f9a3e3
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-16 19:20:43 -0600

    Java 66 Learned to Compare w/ Logical And Operator.

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,14 @@
+// Learned to use the logical and operator (&&) to simplify from two if statements to one. This allows you to compare two different if statements.
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+      return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
