a6c633747c15e8d0e3c978fac240cf6bfd207c65
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 14:07:31 -0600

    Java 95 Learned to Use Odd Numbers in a Loop.

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,15 @@
+//Learned to iterate odd or even numbers with a for loop. Just start odd or even and then do +=2
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
