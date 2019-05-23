02933b9ac0564f5d89aaf18642dfdd61363266fe
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 12:39:52 -0600

    Java 93 Learned about While Loops.

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,10 @@
+//Learned to use a while loop. Until the while statement is not true, the while statement will loop, forever if needed.
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i < 5) {
+  myArray.push(i);
+  i++;
+}
