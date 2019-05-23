eb7f986e5067e41b93dd95585720605a36c03051
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 15:02:33 -0600

    Java 102 Learned to Generate Random Whole Numbers.

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,10 @@
+//Learned how to generate random whole numbers with Math.random(). Ex. Math.floor rounds the number down to its nearest whole number, so then you can multiply the function by a
+//number to get from one minus that number to 0.
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
