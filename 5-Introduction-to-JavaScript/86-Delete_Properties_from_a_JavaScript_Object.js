d242368e13d41a92f53e255220445230e52ef1fd
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 10:39:47 -0600

    Java 86 Learned to Delete Properties from Object.

---
+++ 5-Introduction-to-JavaScript/86-Delete_Properties_from_a_JavaScript_Object.js
@@ -0,0 +1,23 @@
+//Learned how to delete properties from a Java object. Use delete var.property or delete var["property"].
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line.
+delete myDog.tails;
