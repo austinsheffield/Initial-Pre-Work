ea688f01e6f5ac6552ca149f5395e4de708f92be
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 15:15:43 -0600

    Java 104 Learned the Parselnt Function.

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,6 @@
+//Learned to use the parselnt function. Parsels a string and returns an integer. Can use a var.
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+convertToInteger("56");
