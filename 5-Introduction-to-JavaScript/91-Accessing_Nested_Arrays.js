e4a964ec6a7a9570925a565c47f9f8022aaeb803
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 12:09:57 -0600

    Java 91 Learned to Access Nested Arrays.

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,24 @@
+//Learned how to access nested arrays. Ex. var[num].var[num]. DONT FORGET HOW JAVA COUNTS NUMBERS. STARTS AT 0!
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
