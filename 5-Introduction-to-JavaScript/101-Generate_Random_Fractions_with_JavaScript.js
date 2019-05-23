d58d9a780e2eed8ad19d41e4e2f22aae1277a174
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 14:51:44 -0600

    Java 101 Learned how to Generate Random Fractions.

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+//Learned to create random fractions with Math.randowm(). This will generate a random decimal number between 0 and up to 1. Can return a 0 but cannot return 1.
+//Can return a random number with return.
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
