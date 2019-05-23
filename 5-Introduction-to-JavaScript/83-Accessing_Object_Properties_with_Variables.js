d1b290bfb47bc5be941ea6c647be82b80e3cc577
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 10:31:20 -0600

    Java 83 Learned to Accesss Object Properties w/ Variables.

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,12 @@
+//Learned to access object properties with variables. DO NOT USE QUOTES BECAUSE WE ARE LOOKING FOR VALUE OF VARIABLE NOT NAME.
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
