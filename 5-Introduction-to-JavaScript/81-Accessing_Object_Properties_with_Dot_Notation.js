28c6278046fd3e3add52cb34f7358b7360310921
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 10:18:42 -0600

    Java 81 Learned to Access Object Properties with Dot Notation.

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,13 @@
+//Learned to access object properties with dot notation (.), however you can also use bracket notation ([]). Use dot notation when you know what you are trying to access ahead of time.
+//You do not put quotations around the value you are trying to access.
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
