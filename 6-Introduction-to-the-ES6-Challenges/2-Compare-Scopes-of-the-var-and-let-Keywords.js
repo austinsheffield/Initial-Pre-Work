845e83b46ef3a3f5fabb18d3b881e340b09fc104
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 16:34:08 -0600

    ES6 2 Learned Scope of Var and Let

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+//Learned the properties of let keyword within a function. With let, scope is limited to that block, section or expression.
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
