bb2ff965fec248fd2ae9fb925337134b7e4741a8
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 15:58:44 -0600

    Java 107 Learned Multiple Conditional Operators.

---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,7 @@
+//Learned to use multiple conditional operators.
+//Note that for the last operator you should not put any other conditions.
+function checkSign(num) {
+  return num == 0 ? "zero" : num > 0 ? "positive" : "negative";
+}
+
+checkSign(10);
