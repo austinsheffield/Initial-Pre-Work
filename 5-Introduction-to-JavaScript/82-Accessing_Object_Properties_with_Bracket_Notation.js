c6dbf57a7fae6c4c782473763c10c2f983c9632d
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 10:22:24 -0600

    Java 82 Learned to Access Object Prop w/ Bracket Notation.

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,12 @@
+//Learned to access object properties with bracket notation ([]). You need to use this if the object you are trying to access has spaces. MUST BE IN QUOTATIONS.
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
