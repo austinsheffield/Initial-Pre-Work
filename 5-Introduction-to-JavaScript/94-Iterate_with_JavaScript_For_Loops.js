df8015ec3423b7759794f87dc722f99a35250571
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 12:46:59 -0600

    Java 94 Learned to Iterate For Loops.

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,19 @@
+//Learned to iterate with Java for loops. Or how to run the same code multiple times.
+//for ([initialization]; [condition]; [final-expression])
+//initialization is executed ONCE before the loop starts.
+//condition is evaluated EVERYTIME as long as it is true.
+//final-expression is executed at the end of each loop iteration.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
