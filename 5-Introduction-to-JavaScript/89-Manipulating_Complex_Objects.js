34ee2be7d1f0c45b022e0de65bde9d8c17c290a5
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 11:27:05 -0600

    Java 89 Learned to Manipulate Complex Objects.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,25 @@
+//Learned to manipulate complex objects by added more metadata. MAKE SURE TO PUT COMMAS AFTER BRACKETS IN THESE DATA STRUCTURES.
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+  "artist": "Daft Punk",
+  "title": "Homework",
+  "release_year": 1997,
+  "formats": [
+    "CD",
+    "Cassette",
+    "LP"
+  ],
+  }
+];
