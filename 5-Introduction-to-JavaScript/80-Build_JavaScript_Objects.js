6e02a9b46469f5b052585bebed2c13de87d9c536
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 10:15:13 -0600

    Java 80 Learned to Build Java Objects.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,21 @@
+//Learned to build an object, which are similar to an array, but use data in objects with what are called properties. Useful for storing data in a structured way.
+//Using objects you can describe something.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name" : "Fido",
+  "legs" : 4,
+  "tails" : 1,
+  "friends": ["Dogs", "Everyone"]
+
+
+
+};
