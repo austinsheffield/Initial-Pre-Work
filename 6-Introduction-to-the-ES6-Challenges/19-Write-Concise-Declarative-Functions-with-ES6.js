22a542a81f00c3224a81c824051b611f35d991ec
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-24 10:07:25 -0600

    ES6 19 Learned to Write Concise Declarative Functions.

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,12 @@
+//Learned how to write shorthand for declarative functions. Ex. var(var2){code here.}
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
