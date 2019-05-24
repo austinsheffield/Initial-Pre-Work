c3564df5f1845a7673b56792b40560e24fa05eed
austin.sheffield <austin.taylor.sheffield@gmail.com>
2019-05-23 18:11:35 -0600

    ES6 12 Learned to use Destructuring Assignment to Assign Variables from Objects.

---
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,15 @@
+//Learned to use destructuring assignment to assign variables from objects. This syntax allows for assigning values taken from objects to varibles.
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const {tomorrow:tempOfTomorrow} = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
